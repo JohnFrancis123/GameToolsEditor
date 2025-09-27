d4bc92c5dd5cd8d0be4d590b595aae3fad944424
johnF <franci24@sheridancollege.ca>
2025-09-27 17:56:19 -0400

    Finalized saving and loading for Celestial Bodies. Apparently raw data works better when we're working with bytes, as opposed to strings.

--- Editor/Engine/CelestialBody.cs
+++ Editor/Engine/CelestialBody.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Editor.Engine.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct2D1.Effects;
+using System;
+using System.IO;
 
 namespace Editor.Engine
 {
@@ -7,15 +13,52 @@ namespace Editor.Engine
         public Models CelestialBodyModel { get; set; }
         public byte BodyType { get; set; } //0 for sun, 1 for planet, 2 for moon
 
-        CelestialBody(Models _celestialBodyModel, byte _bodyType)
+        public CelestialBody() 
+        {
+
+        }
+
+        public CelestialBody(Models _celestialBodyModel, byte _bodyType)
         {
             CelestialBodyModel = _celestialBodyModel;
             BodyType = _bodyType;
         }
 
-        public void Render()
+        public void Render(Matrix _view,
+                           Matrix _projection)
         {
-            //
+            if(BodyType == 0)
+            {
+                Vector3 currRot = CelestialBodyModel.Rotation;
+                currRot.Y += 0.005f;
+                CelestialBodyModel.Rotation = currRot;
+            }
+
+            CelestialBodyModel.Render(_view, _projection);
+        }
+
+        private void Orbit()
+        {
+
+        }
+
+        public void Serialize(BinaryWriter _stream)
+        {
+            CelestialBodyModel.Serialize(_stream);
+            //_stream.Write(BodyType.ToString());
+            _stream.Write(BodyType);
+        }
+
+        public void Deserialize(BinaryReader _stream, ContentManager _content)
+        {
+            Models m = new();
+            m.Deserialize(_stream, _content);
+            CelestialBodyModel = m;
+
+            //string bodyType = _stream.ReadString();
+            //BodyType = ((byte)bodyType[0]);
+
+            BodyType = _stream.ReadByte();
         }
     }
 }
--- Editor/Engine/Level.cs
+++ Editor/Engine/Level.cs
@@ -14,6 +14,8 @@ namespace Editor.Engine
 
         // Members
         private List<Models> m_models = new();
+        private List<CelestialBody> m_bodies = new();
+
         private Camera m_camera = new(new Vector3(0, 2, 2), 16.0f / 9.0f); //my resolution is 16x10
 
         public Level()
@@ -24,9 +26,17 @@ namespace Editor.Engine
         {
             Models sun = new(_content, "obj/Sphere", "obj/worlddiffuse", "MyShader", Vector3.Zero, 2.0f);
             sun.SetShader(_content.Load<Effect>("MyShader"));
-            AddModel(sun);
+
+            CelestialBody body = new CelestialBody(sun, 0);
+
+            AddBody(body);
+            //AddModel(sun);
         }
 
+        public void AddBody(CelestialBody _body)
+        {
+            m_bodies.Add(_body);
+        }
         public void AddModel(Models _model)
         {
             m_models.Add(_model);
@@ -34,32 +44,53 @@ namespace Editor.Engine
 
         public void Render()
         {
-            foreach (Models m in m_models)
+            //foreach (Models m in m_models)
+            //{
+            //    m.Render(m_camera.View, m_camera.Projection);
+            //}
+
+            foreach (CelestialBody c in m_bodies)
             {
-                m.Render(m_camera.View, m_camera.Projection);
+                c.Render(m_camera.View, m_camera.Projection);
             }
         }
 
         public void Serialize(BinaryWriter _stream)
         {
-            _stream.Write(m_models.Count);
-            foreach (var model in m_models)
+            //_stream.Write(m_models.Count);
+            //foreach (var model in m_models)
+            //{
+            //    model.Serialize(_stream);
+            //}
+
+            _stream.Write(m_bodies.Count);
+            foreach (var body in m_bodies)
             {
-                model.Serialize(_stream);
+                body.Serialize(_stream);
             }
+
             m_camera.Serialize(_stream);
         }
 
         public void Deserialize(BinaryReader _stream, ContentManager _content)
         {
-            int modelCount = _stream.ReadInt32();
-            for (int count = 0; count < modelCount; count++) 
+            //int modelCount = _stream.ReadInt32();
+            //for (int count = 0; count < modelCount; count++) 
+            //{
+            //    Models m = new();
+            //    m.Deserialize(_stream, _content);
+            //    m_models.Add(m);
+            //}
+
+            int bodyCount = _stream.ReadInt32();
+            for (int count = 0; count < bodyCount; count++)
             {
-                Models m = new();
-                m.Deserialize(_stream, _content);
-                m_models.Add(m);
+                CelestialBody c = new();
+                c.Deserialize(_stream, _content);
+                m_bodies.Add(c);
             }
             m_camera.Deserialize(_stream, _content);
+
         }
     }
 }
